48c97e0e664283f1ae66be2ba25eb03f7582a876
fantastickford1 <carlitos_zenteno@hotmail.com>
2016-01-17 17:44:43 -0600

    :package: *Can open files in the right way *Has all the complexity formulas

--- Interface/src/Core/Controller.java
+++ Interface/src/Core/Controller.java
@@ -3,6 +3,10 @@ package Core;
 import Sorting.*;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
@@ -11,14 +15,32 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
+
+    @FXML
+    TextArea textarea;
+    @FXML
+    TableView table;
+    @FXML
+    TableColumn tableColumnM,tableColumnOp,tableColumnIt,tableColumnIn;
+    @FXML
+    ChoiceBox<String> choice;
+
+    //Sorting objects//
     Burbuja bubble;
-    Merge mergeSort;
-    OrdenamientoInsercion insertion;
+    //Merge mergeSort;
+    InsercionYSeleccion insertion;
+    Quicksort quicksort;
+    //Objects//
     FileChooser fileChooser;
     File file;
     FileReader fr;
     BufferedReader br;
     Stage stage;
+    //data//
+    String line;
+    String data;
+    String[] allData;
+    int[] allInt;
 
     @FXML private void openFile(){
         fileChooser = new FileChooser();
@@ -33,32 +55,51 @@ public class Controller implements Initializable {
     }
 
     private void ReadFile(File file){
-        String text;
-        String all = "";
+        data = "";
         try {
             fr = new FileReader(file);
             br = new BufferedReader(fr);
 
-            while ((text = br.readLine()) != null){
-                all = all + text;
+            while ((line = br.readLine()) != null){
+                data += line + ",";
             }
+
+            allData = data.split(",");
+            allInt = new int[allData.length];
+            int a = 0;
+            for (String sw: allData) {
+                allInt[a] = Integer.parseInt(sw);
+                a++;
+            }
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
-        }finally {
-            try {
-                br.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
-        System.out.println(all);
+        textarea.setEditable(false);
+        textarea.setText("");
+        for (int w: allInt) {
+            textarea.appendText(String.valueOf(w) + "\n");
+        }
+        complex();
+    }
+
+    private void complex(){
+        double burbuja = Math.pow(allInt.length,2);
+        double insercion = Math.pow(allInt.length,2)/4;
+        double merge = allInt.length * (Math.log(allInt.length)/Math.log(2));
+        double quicksort = Math.pow(allInt.length,2);
+        double quicksortProm = allInt.length * (Math.log(allInt.length)/Math.log(1));
+    }
+
+    @FXML private void sort(){
+
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        choice.getItems().addAll("Burbuja","Insercion","Merge","Quicksort");
     }
 
 }
---
+++ Interface/src/Sorting/InsercionYSeleccion.java
@@ -0,0 +1,43 @@
+package Sorting;
+
+import java.util.*;
+public class InsercionYSeleccion{
+
+    public void InsercionSort(int matriz[]){
+        int i, temp, j;
+
+        for (i= 1; i < matriz.length; i++){
+            temp = matriz[i];
+            j = i-1;
+
+            while ((matriz[j] >temp) && (j>= 0 )){
+                matriz[j + 1] = matriz[j];
+                j--;
+            }
+
+            matriz[j + 1] = temp;
+        }
+    }
+
+    public void SeleccionSort(int[] matriz){
+        int i, j, k, p, buffer, limit = matriz.length-1;
+
+        for (k = 0; k <= limit; k++){
+
+            p = k;
+
+            for (i = k+1; k < limit; i++){
+                if (matriz[i] < matriz[p]){
+                    p = i;
+                }
+            }
+            if (p != k){
+                buffer = matriz[p];
+                matriz[p] = matriz[k];
+                matriz[k] = buffer;
+            }
+        }
+    }
+
+
+}
